ba724b3e025efe1a56218afcde0e7ffa97608619
Naveendevarajan <naveendevarajan4@gmail.com>
2024-07-30 03:42:49 -0700

    Factorial

---
+++ src/Factorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        Scanner sc=new Scanner(System.in);
+        int number= sc.nextInt();
+        int factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            factorial=factorial*i;
+        }
+        System.out.println("Factorial of"+number+"is:"+factorial);
+    }
+}
