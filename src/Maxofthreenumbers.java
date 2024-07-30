cc3beecba74fb4dd5c6afefc5e66279cb786cdcb
Naveendevarajan <naveendevarajan4@gmail.com>
2024-07-30 04:52:33 -0700

    Max of three numbers

--- src/Maxofthreenumbers.java
+++ src/Maxofthreenumbers.java
@@ -1,20 +1,21 @@
+import java.util.Arrays;
+
 public class Maxofthreenumbers {
     public static void main(String[] args) {
         int i, j, temp;
-        int []arr={40,50,90,60};
-        for (i = 0; i < arr.length; i++) {
-            for (j = 0; j < arr.length;j++){
-                if (arr[i] > arr[j]) {
-                    temp= arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+        int []arr={1452,4574,12547,45458,1245115,457451,325415,32541,25415};
+        for (i = 0; i < arr.length; i++) {  // i=0
+            for (j = i; j < arr.length;j++) { // j=0
+                if (arr[i] > arr[j]) { //
+                    temp= arr[i]; //
+                    arr[i] = arr[j]; //
+                    arr[j] = temp; //
+                    //{90,60}
                 }
-
-
             }
         }
-        System.out.println("1st largest" + arr[arr.length-3]);
-        System.out.println("2nd largest" + arr[arr.length-2]);
-        System.out.println("3rd largest" + arr[arr.length-1]);
+        System.out.println("1st largest"+arr[arr.length-1]);
+        System.out.println("2nd largest"+arr[arr.length-2]);
+        System.out.println("3rd largest"+arr[arr.length-3]);
     }
 }
