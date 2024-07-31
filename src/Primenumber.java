e3e40c6b207ea16ba8cc9dea22b6e54244e73013
Naveendevarajan <naveendevarajan4@gmail.com>
2024-07-31 02:55:03 -0700

    prime numbers

--- src/Primenumber.java
+++ src/Primenumber.java
@@ -1,24 +1,19 @@
-public class Primenumber { public static void main (String[] args)
-{
-    int i =0;
-    int j =0;
-    String  primeNumbers = "";
-    for (i = 1; i <= 484; i++)
+public class Primenumber {
+    public static void main (String[] args)
     {
-        int counter=0;
-        for(j =i; j>=1; j--)
-        {
-            if(i%j==0)
-            {
-                counter = counter + 1;
+        int number=484;
+        for(int i=2;i<number;i++){
+            int count=0;
+            for(int j=2;j<=i;j++){
+                if(i%j==0){
+                    count++;
+                }
+            }
+            if(count<2){
+                System.out.println(i);
             }
         }
-        if (counter ==2)
-        {
-            primeNumbers = primeNumbers + i + " ";
-        }
+
+
     }
-    System.out.println("Prime numbers from 1 to 484 are :");
-    System.out.println(primeNumbers);
-}
 }
