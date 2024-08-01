c7f52db5b0ba5531c1ccb71ad23d702096595fec
Naveendevarajan <naveendevarajan4@gmail.com>
2024-08-01 02:09:14 -0700

    Grade

--- src/Grade.java
+++ src/Grade.java
@@ -5,7 +5,7 @@ public class Grade {
         Scanner sc = new Scanner(System.in);
         int mark = sc.nextInt();
         if (mark >500) {
-            System.out.println("the maximum mark is 500");
+            System.out.println("The maximum mark is 500");
         } else if (mark>=450 && mark<=500){
             System.out.println("grade is A");
         } else if (mark>=400&&mark<450){
