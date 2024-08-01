a9834b1b54d87804df7dbeae602b145f937c6e86
Naveendevarajan <naveendevarajan4@gmail.com>
2024-08-01 02:18:06 -0700

    Smallest and largest numbers in a given array

--- src/Smallestandlargestnumber.java
+++ src/Smallestandlargestnumber.java
@@ -5,7 +5,7 @@ public class Smallestandlargestnumber {
             int []arr={42,445547,784857,145142,78448};
             for (i = 0; i < arr.length; i++) {
                 for (j = i; j < arr.length;j++){
-                    if (arr[i] < arr[j]) {
+                    if (arr[i] >arr[j]) {
                         temp= arr[i];
                         arr[i] = arr[j];
                         arr[j] = temp;
@@ -14,8 +14,8 @@ public class Smallestandlargestnumber {
 
                 }
             }
-            System.out.println("largest:" + arr[arr.length-3]);
-            System.out.println("smallest:" + arr[arr.length-1]);
+            System.out.println("largest:" + arr[arr.length-1]);
+            System.out.println("smallest:" + arr[arr.length-5]);
         }
     }
 }
