5699d50e97f1de04fbba92e0ddcbe205765a21ad
Naveendevarajan <naveendevarajan4@gmail.com>
2024-08-08 00:02:01 -0700

    Invalid Input  Exception

---
+++ src/Exception/IvalidInputException.java
@@ -0,0 +1,17 @@
+package Exception;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class IvalidInputException {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        try {
+            System.out.println("Enter Input:");
+            int input= sc.nextInt();
+            System.out.println("The Input is:"+input);
+        }
+        catch (InputMismatchException a){
+            System.out.println("Enter the valid Input:"+a);
+
+        }
+    }
+}
