4fbec182f92c250c696b4765d64803836fbbcf6b
Naveendevarajan <naveendevarajan4@gmail.com>
2024-08-22 02:13:11 -0500

    sum of first and last digit

---
+++ src/taskUsingnumbers/sumOfFirstAndLastDigit.java
@@ -0,0 +1,24 @@
+package taskUsingnumbers;
+import java.util.Scanner;
+public class sumOfFirstAndLastDigit {
+    public int sumFirstAndLastDigit(int number) {
+        int firstdigit=number;
+            while (firstdigit >=9) {
+                firstdigit = firstdigit / 10;
+            }
+            int lastdigit = number % 10;
+            if(number<0) {
+                System.out.println("Enter number greater than 0");
+            return -1;
+        }
+        return firstdigit+lastdigit;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter a Number:");
+        int inputnumber=scanner.nextInt();
+        sumOfFirstAndLastDigit sumoffirstandlastdigit=new sumOfFirstAndLastDigit();
+        System.out.println(sumoffirstandlastdigit.sumFirstAndLastDigit(inputnumber));
+    }
+}
