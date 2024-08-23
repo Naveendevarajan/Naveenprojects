3a537e7f1ae55b80cb95fcc7acccf325e97c0b6b
Naveendevarajan <naveendevarajan4@gmail.com>
2024-08-23 04:40:44 -0500

    print the given pattern

---
+++ src/Task23aug/pass.java
@@ -0,0 +1,15 @@
+package Task23aug;
+
+public class pass {
+
+    public static void main(String[] args) {
+        for(int i=0;i<=50;i++){
+            if (i%5==0)
+                System.out.println("pass");
+            else
+                System.out.println(i);
+        }
+
+    }
+
+}
