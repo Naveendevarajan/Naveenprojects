72b490349e02a29a3ef510dd705b57381098af9b
Naveendevarajan <naveendevarajan4@gmail.com>
2024-08-26 16:57:31 +0530

    Banking System

--- src/taskUsingnumbers/HDFC.java
+++ src/taskUsingnumbers/HDFC.java
@@ -4,7 +4,7 @@ public class HDFC extends Visa {
     double balance=10000;
     @Override
     protected boolean authenticate(String cardNumber, int pin) {
-        return cardNumber.equals(hdfccardnumber)&&pin== hdfcPin;
+        return cardNumber.equals(hdfccardNumber)&&pin== hdfcPin;
     }
     public void withDrawProcess(String cardNumber, int pin, double requestedamount){
         if(authenticate(cardNumber,pin)){
