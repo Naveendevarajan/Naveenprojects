3468829b1427cb2580a212f279d320b1b9682c33
Naveendevarajan <naveendevarajan4@gmail.com>
2024-08-27 16:56:38 +0530

    Boost Company

--- src/Task23aug/Shop.java
+++ src/Task23aug/Shop.java
@@ -1,16 +1,13 @@
 package Task23aug;
 
-public class Shop extends dealer{
-    int boostMRP=100;
-    int  boostSellingPrice=98;
-    public Shop(int boostMRP){
-        super(boostMRP);
-        System.out.println("The MRP of shop "+this.boostMRP);
-        System.out.println("The MRP of Dealer "+super.boostMRP);
-    }
+public class Shop extends dealer {
+    int boostMRP = 100;
+    int boostSellingPrice = 98;
 
-    public static void main(String[] args) {
-        Shop shop = new Shop(110);
+    public Shop(int boostMRP) {
+        super(boostMRP);
+        System.out.println("The MRP of shop " + this.boostMRP);
+        System.out.println("The MRP of Dealer " + super.boostMRP);
     }
 
 }
