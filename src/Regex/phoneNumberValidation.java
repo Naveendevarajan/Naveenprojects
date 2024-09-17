be3ccf27c4944018e5a368d48d5d7b7ccbf37f9f
Naveendevarajan <naveendevarajan4@gmail.com>
2024-09-17 16:42:01 +0530

    phone number validation

--- src/Regex/phoneNumberValidation.java
+++ src/Regex/phoneNumberValidation.java
@@ -9,12 +9,15 @@ public class phoneNumberValidation {
         String validPhone = "\\(\\+\\d{2}\\)\\s\\d{5}-\\d{5} | \\d{3}-\\d{3}-\\d{4}";
         Pattern pattern = Pattern.compile(validPhone);
         Matcher matcher = pattern.matcher(text);
-        String result = "";
-        while (matcher.find()) {
-            result += matcher.group() + " or ";
+        StringBuilder result = new StringBuilder();
+        while (matcher.find()) {{
+            if(!result.isEmpty())
+                result.append("or");
+        }
+            result.append(matcher.group());
         }
         if(!result.isEmpty()) {
-            return result+" if you prefer the first occurrence or modify the code to handle multiple numbers.";
+            return result.toString();
         }
         else
             return "NULL";
@@ -22,8 +25,6 @@ public class phoneNumberValidation {
     public static void main(String[] args) {
         phoneNumberValidation c = new phoneNumberValidation();
         System.out.println(c.extractPhoneNumbers("Call me at (+91) 23456-72890 or at 987-424-4124"));
-        System.out.print(c.extractPhoneNumbers("NO Phone number"));
-
 
     }
 
